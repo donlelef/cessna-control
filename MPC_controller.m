0d987f23485d858fac69feacf00e02af84ec1f89
donlelef <donlelef@gmail.com>
2016-05-24 00:12:15 +0200

    "Fixed bugs on MPC control and input constraints added"

--- MPC_controller.m
+++ MPC_controller.m
@@ -1,4 +1,4 @@
-function [ u ] = MPC_controller( A, B, Q, R, S, N, umin, umax, x )
+function [ u ] = MPC_controller( A, B, Q, R, S, N, umin, umax, xmin, xmax, x )
 %MPC_CONTROLLER return the input as computed by an mpc aontroller
 %   Detailed explanation goes here
 
@@ -29,11 +29,27 @@ Qc = blkdiag(Qc, S);
 
 Rc = kron(eye(N), R);
 
-H = Bc' * Qc * Bc + Rc;
+H = 2 * Bc' * Qc * Bc + Rc;
 f = 2 * x' * Ac' * Qc * Bc;
 umin_constr = umin * ones(N, 1);
 umax_constr = umax * ones(N, 1);
 
-u = quadprog(H, f, [], [], [], [], umin_constr, umax_constr, [], options);
+[A_leq, b_leq] = build_inequality(Ac, Bc, N, xmin, xmax, x);
+
+[u, ~, flag] = quadprog(H, f, A_leq, b_leq, [], [], umin_constr, umax_constr, [], options);
 u = u(1);
+end
+
+function [A, b] = build_inequality(Ac, Bc, N, x_min, x_max, x)
+x_max_vect = repmat(x_max, N, 1);
+A = Bc;
+b = x_max_vect - Ac * x;
+
+x_min_vect = repmat(x_min, N, 1);
+A = [A; -Bc];
+b = [b; - x_min_vect + Ac * x];
+
+A = A(b > -inf & b < inf, :);
+b = b(b > -inf & b < inf);
+
 end
