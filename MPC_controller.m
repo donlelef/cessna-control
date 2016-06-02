0245487e31f16eb4b61979432c4d53fae6f1bf22
donlelef <donlelef@gmail.com>
2016-06-02 23:30:20 +0200

    "Several bug fixed."

--- MPC_controller.m
+++ MPC_controller.m
@@ -4,10 +4,11 @@ function [ u ] = MPC_controller( sys, Q, R, S, N, umin, umax, uslope_min, uslope
 
 options = optimset('quadprog');
 options = optimset(options, 'LargeScale', 'off', 'Display' , 'off');
+rho = 100;
 
 Ac = [];
 for i = 1:N
-    Ac = [Ac;sys.a^i];
+    Ac = [Ac; sys.a^i];
 end
 
 Bc = [];
@@ -28,15 +29,13 @@ Qc = kron(eye(N-1), Q);
 Qc = blkdiag(Qc, S);
 
 Rc = kron(eye(N), R);
-H = 2 * Bc' * Qc * Bc + Rc;
+H = 2 * (Bc' * Qc * Bc + Rc);
 H = blkdiag(H, 0);
 f = 2 * x' * Ac' * Qc * Bc;
-f = [f 1e12];
-umin_constr = umin * ones(N, 1);
-umin_constr = [umin_constr; 0];
+f = [f rho];
 
-umax_constr = umax * ones(N, 1);
-umax_constr = [umax_constr; +inf];
+umin_constr = [umin .* ones(N, 1); 0];
+umax_constr = [umax .* ones(N, 1); inf];
 
 [A_state, b_state] = build_state_contraint_matrix(Ac, Bc, N, xmin, xmax, x);
 [A_slope, b_slope] = build_slope_contraint_matrix(sys.Ts, N, uslope_min, uslope_max, uprec);
@@ -44,9 +43,15 @@ A_leq = [A_state; A_slope];
 b_leq = [b_state; b_slope];
 [A_leq, b_leq] = remove_infinite_constraints(A_leq, b_leq);
 
-[u, ~, flag] = quadprog(H, f, A_leq, b_leq, [], [], umin_constr, umax_constr, [], options);
+[u, cost, flag] = quadprog(H, f, A_leq, b_leq, [], [], umin_constr, umax_constr, [], options);
+cost = cost - u(11) * rho + x' * Ac' * Qc * Ac * x;
 handle_error(flag);
-u = u(1);
+if(cost < 0)
+    state = x
+    input = u(1:10)
+    slack = u(11)
+end
+u = [u(1), cost]';
 end
 
 function handle_error(flag)
@@ -59,13 +64,13 @@ end
 end
 
 function [A, b] = build_state_contraint_matrix(Ac, Bc, N, x_min, x_max, x)
+ev = repmat([0 0 0 1]', N, 1);
 x_max_vect = repmat(x_max, N, 1);
-A = Bc;
 b = x_max_vect - Ac * x;
-A = [A repmat([0 0 0 -1]', N, 1)];
+A = [Bc -ev];
 
 x_min_vect = repmat(x_min, N, 1);
-A = [A; -[Bc repmat([0 0 0 -1]', N, 1)]];
+A = [A; -[Bc ev]];
 b = [b; - x_min_vect + Ac * x];
 end
 
